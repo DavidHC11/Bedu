80a1d810c264ddeea76c2674ffbcf37803dce7d9
DavidHC11 <67530511+DavidHC11@users.noreply.github.com>
2020-10-28 19:55:51 -0600

    Update retoclase1.sql

--- retoclase1.sql
+++ retoclase1.sql
@@ -11,3 +11,11 @@ select nombre from puesto where salario >10000;
 select nombre from articulo where precio >1000 and iva >100;
  select * from venta where id_articulo in (135, 963) and id_empleado in (835,369);
 
+#int Datos numéricos enteros
+#varchar	Cadenas de caracteres variables
+#timestamp	Fechas y hora
+#double	Datos numéricos con decimales
+
+select * from tienda 
+order by salario desc
+limit 5;
